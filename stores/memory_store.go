9dbda665d9727f540a64a46b443b5f0cc9dfef69
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-05-18 11:01:53 +0300

    Used the concurrent map implemented by dropbox.

--- stores/memory_store.go
+++ stores/memory_store.go
@@ -1,61 +1,45 @@
 package stores
 
 import (
+	"github.com/dropbox/godropbox/container/concurrent"
 	"github.com/vlad-doru/experimento/interfaces"
-	"sync"
 )
-
-type expGroups struct {
-	mapping map[string]string
-	mutex   sync.RWMutex
-}
 
 // MemoryStore implements the Store interface defined by the experimento project
 // by holding all the necessary data in memory, in a map.
 type MemoryStore struct {
 	// Maps entity_id -> exp_id -> group
-	mapping map[string]*expGroups
-	mutex   sync.RWMutex
+	mapping concurrent.Map
 }
 
 // NewMemoryStore returns a new, empty MemoryStore.
 func NewMemoryStore() *MemoryStore {
-	mapping := make(map[string]*expGroups)
-	return &MemoryStore{mapping, sync.RWMutex{}}
+	mapping := concurrent.NewMap()
+	return &MemoryStore{mapping}
 }
 
 // SetExperimentGroup sets the experiment group for a particular entity and
 // experiment, by saving the group id in memory.
 func (store *MemoryStore) SetExperimentGroup(entityID, expID, group string) error {
-	store.mutex.RLock()
-	m, ok := store.mapping[entityID]
-	store.mutex.RUnlock()
+	m, ok := store.mapping.Get(entityID)
 	if ok == false {
-		m = &expGroups{map[string]string{}, sync.RWMutex{}}
-		store.mutex.Lock()
-		store.mapping[entityID] = m
-		store.mutex.Unlock()
+		m = concurrent.NewMap()
+		store.mapping.Set(entityID, m)
 	}
-	m.mutex.Lock()
-	m.mapping[expID] = group
-	m.mutex.Unlock()
+	m.(concurrent.Map).Set(expID, group)
 	return nil
 }
 
 // GetExperimentGroup returns the id of the group in which a specific entity id
 // finds itself, in a specific experiment.
 func (store *MemoryStore) GetExperimentGroup(entityID, expID string) (string, error) {
-	store.mutex.RLock()
-	m, ok := store.mapping[entityID]
-	store.mutex.RUnlock()
+	m, ok := store.mapping.Get(entityID)
 	if ok == false {
 		return "", interfaces.NoGroupSet{EntityID: entityID, ExperimentID: expID}
 	}
-	m.mutex.RLock()
-	groupID, ok := m.mapping[expID]
-	m.mutex.RUnlock()
+	groupID, ok := m.(concurrent.Map).Get(expID)
 	if ok == false {
 		return "", interfaces.NoGroupSet{EntityID: entityID, ExperimentID: expID}
 	}
-	return groupID, nil
+	return groupID.(string), nil
 }
