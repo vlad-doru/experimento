848aa67b1f4cb6c4c295f804fd690a9a4fd78623
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-06-13 13:50:56 +0300

    Removed the useless component, we will start to build our own.

--- ui/src/components/Header/Header.js
+++ ui/src/components/Header/Header.js
@@ -5,13 +5,6 @@ import classes from './Header.scss'
 export const Header = () => (
   <div>
     <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName={classes.activeRoute}>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName={classes.activeRoute}>
-      Counter
-    </Link>
   </div>
 )
 
