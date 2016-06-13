a6dba9d0e3298a53cd88aef7e32ea784f866b93e
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-06-13 14:00:11 +0300

    Move to ES6 defintions and remove too much complexity.

--- ui/src/components/Header/Header.js
+++ ui/src/components/Header/Header.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import classes from './Header.scss'
 
-export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-  </div>
-)
+class Header extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>React Redux Starter Kit</h1>
+      </div>
+    )
+  }
+}
 
 export default Header
--- ui/src/routes/Home/index.js
+++ ui/src/routes/Home/index.js
@@ -1,6 +1,19 @@
-import HomeView from './components/HomeView'
+import React from 'react'
+import DuckImage from './assets/Duck.jpg'
 
-// Sync route definition
+class HomeView extends React.Component {
+  render () {
+    return (
+      <div>
+        <h4>Welcome!</h4>
+        <img
+          alt='This is a duck, because Redux!'
+          src={DuckImage} />
+      </div>
+    )
+  }
+}
+
 export default {
   component: HomeView
 }
