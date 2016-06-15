44074ee3a8c40e7a1b11039a471cefba17f1569e
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-06-15 14:16:29 +0300

    Now we have a nice list going on.

--- ui/src/components/VariableInfo/VariableInfo.js
+++ ui/src/components/VariableInfo/VariableInfo.js
@@ -3,6 +3,17 @@ import TextField from 'material-ui/TextField';
 import Slider from 'material-ui/Slider';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import ActionList from 'material-ui/svg-icons/action/list';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
+import ActionAssignment from 'material-ui/svg-icons/action/assignment';
+import NavigationMoreVert from 'material-ui/svg-icons/navigation/more-vert';
+import Avatar from 'material-ui/Avatar';
+import Subheader from 'material-ui/Subheader';
+import {blue500, darkBlack, lightBlack} from 'material-ui/styles/colors';
+import __ from 'lodash';
 
 export class ExperimentVariables extends React.Component {
   constructor(props) {
@@ -77,6 +88,36 @@ export class ExperimentVariables extends React.Component {
   }
 
   render () {
+
+    let vars =  __.map(this.state.variables,
+      (value, key) => {
+        console.log(key, value)
+        return (
+          <ListItem
+            style={{
+              backgroundColor: 'white',
+            }}
+            innerDivStyle={{
+              margin: '50px 1-px',
+              border: '1px solid white',
+            }}
+            leftAvatar={<Avatar icon={<ActionAssignment />}/>}
+            rightIcon={<NavigationMoreVert />}
+            primaryText={key}
+            secondaryText={
+              <p>
+                <span style={{color: darkBlack}}>
+                  {value.length ? "Possible values: " :
+                                  "No values were defined"}
+                </span><br />
+                {value.join(", ")}
+              </p>
+            }
+          />
+        )
+      }
+    );
+
     return (
       <div>
         <TextField
@@ -98,7 +139,15 @@ export class ExperimentVariables extends React.Component {
             onMouseUp={this._addVariable}>
           <ContentAdd />
         </FloatingActionButton>
-        {JSON.stringify(this.state.variables)}
+        <br/>
+
+        <div style={{textAlign: 'left'}}>
+        <List style={{border: 'none'}}>
+        <Subheader inset={true}>Variables</Subheader>
+          {vars}
+        </List>
+        </div>
+
       </div>
     )
   }
