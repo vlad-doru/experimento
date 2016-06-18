055a13b8b5aee11a612d2e47de91537c4dfc31f8
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-06-18 17:03:37 +0300

    Implemented the interfaces for the server.

--- repositories/immutable_repository.go
+++ repositories/immutable_repository.go
@@ -2,9 +2,7 @@ package repositories
 
 import (
 	"github.com/vlad-doru/experimento/data"
-	// "golang.org/x/net/context"
-	// "google.golang.org/grpc"
-	// "fmt"
+	"golang.org/x/net/context"
 )
 
 type ImmutableRepository struct {
@@ -16,32 +14,31 @@ func NewImmutableRepository() *ImmutableRepository {
 	return &ImmutableRepository{experiments}
 }
 
-// CreateExperiment saves an experiment with the given description in the repository.
-// func (repository *MemoryRepository) CreateExperiment(exp data.Experiment) error {
-// 	_, ok := repository.experiments[exp.Info.Id]
-// 	if ok == true {
-// 		return fmt.Errorf("There is already an experiment with the given id: %s", exp.Info.Id)
-// 	}
-// 	repository.experiments[exp.Info.Id] = exp
-// 	return nil
-// }
-//
-// // GetExperiment returns the experiment description of the experiment with the
-// // specified experiment id.
-// func (repository *MemoryRepository) GetExperiment(expID string) (data.Experiment, error) {
-// 	exp, ok := repository.experiments[expID]
-// 	if ok == false {
-// 		return data.Experiment{}, fmt.Errorf("The experiment with id %s does not exist.", expID)
-// 	}
-// 	return exp, nil
-// }
-//
-// // DestroyExperiment removes the experiment with the given id from the repository.
-// func (repository *MemoryRepository) DestroyExperiment(expID string) {
-// 	delete(repository.experiments, expID)
-// }
-//
-// // GetExperiments returns all the current experiments form the memory.
-// func (repository *MemoryRepository) GetExperiments() (map[string]data.Experiment, error) {
-// 	return repository.experiments, nil
-// }
+func (repository *ImmutableRepository) SaveExperiment(c context.Context, exp *data.Experiment) (*data.Response, error) {
+	_, ok := repository.experiments[exp.Info.Id]
+	if ok == true {
+		return &data.Response{
+			Ok:    false,
+			Error: "There is already an experiment with the given id!",
+		}, nil
+	}
+	repository.experiments[exp.Info.Id] = *exp
+	return &data.Response{
+		Ok: true,
+	}, nil
+}
+
+func (repository *ImmutableRepository) DropExperiment(c context.Context, info *data.ExperimentInfo) (*data.Response, error) {
+	delete(repository.experiments, info.Id)
+	return &data.Response{
+		Ok: true,
+	}, nil
+}
+
+func (repository *ImmutableRepository) GetExperiments(context.Context, *data.Void) (*data.Experiments, error) {
+	result := &data.Experiments{Experiments: make(map[string]*data.Experiment)}
+	for key, value := range repository.experiments {
+		result.Experiments[key] = &value
+	}
+	return result, nil
+}
