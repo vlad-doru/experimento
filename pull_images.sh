6fd90a6fe15408b283941b4cfe22380bf8c0e754
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-06-23 14:47:31 +0300

    Added an utility script to pull images faster.

---
+++ pull_images.sh
@@ -0,0 +1,4 @@
+gcloud docker pull gcr.io/disertatie-1351/ui:latest && \
+gcloud docker pull gcr.io/disertatie-1351/experimento:latest && \
+gcloud docker pull gcr.io/disertatie-1351/repository:latest && \
+gcloud docker pull gcr.io/disertatie-1351/store:latest
